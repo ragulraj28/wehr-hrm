19c3979d70bde4a8a50087e59f1d40c8ebf994e4
ragulraj <rraj9350@gmail.com>
2025-04-03 16:37:57 +0530

    css fixes

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from "vite-plugin-svgr"
-import {visualizer} from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    svgr(),
-    visualizer({open: true})
+    svgr()
   ],
 })
