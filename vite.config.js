c57a1d856e323a71117b20b57b187282971e6f03
ragulraj <rraj9350@gmail.com>
2025-04-03 14:47:32 +0530

    change app title

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import svgr from "vite-plugin-svgr";
+import svgr from "vite-plugin-svgr"
+import {visualizer} from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    svgr()
+    svgr(),
+    visualizer({open: true})
   ],
 })
